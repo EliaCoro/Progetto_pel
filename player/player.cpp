a3f63a7cfede3f0f127352076a1b9caedf0211dc
EliaCoro <elia.coro02@gmail.com>
2022-04-12 15:51:51 +0200

    Player

---
+++ player/player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by elia on 12/04/22.
+//
+
+#include "player.h"
---
+++ player/player.h
@@ -0,0 +1,14 @@
+//
+// Created by elia on 12/04/22.
+//
+
+#ifndef PROGETTO_PEL_PLAYER_H
+#define PROGETTO_PEL_PLAYER_H
+
+
+class player {
+
+};
+
+
+#endif //PROGETTO_PEL_PLAYER_H
